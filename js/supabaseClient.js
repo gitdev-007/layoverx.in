6e0646f66eaa56c3b72040d0d3eef9d691c0aa74
Dev Tinker <devtinker007@gmail.com>
2026-04-27 04:57:24 +0530

    update

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,4 +1,4 @@
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   "https://wxgcpopghopfyuaqbbfb.supabase.co",
   "sb_publishable_Sq0N68LRTlfMDXX1KNFABQ_kgO56I-_"
 );
